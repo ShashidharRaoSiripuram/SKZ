ee8a88ef908c288fec3d0bbda06c6cd58df0dc57
ShashidharRaoSiripuram <shashi@zplus.care>
2025-07-16 16:00:24 +0530

    Update PersonalInfo.tsx

--- app/profile/PersonalInfo.tsx
+++ app/profile/PersonalInfo.tsx
@@ -183,8 +183,8 @@ export default function PersonalInfo() {
                 onChange={handleInputChange}
                 className="w-full px-3 py-2 border border-gray-300 rounded-lg text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
               >
-                <option value="Bachelors">Bachelor's</option>
-                <option value="Masters">Master's</option>
+                <option value="Bachelors">Bachelor&aposs</option>
+                <option value="Masters">Master&aposs</option>
                 <option value="PhD">PhD</option>
                 <option value="Diploma">Diploma</option>
               </select>
