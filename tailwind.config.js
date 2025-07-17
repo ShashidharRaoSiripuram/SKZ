5e2957d76eb7523fdc5075840ff01c6f939822b7
ShashidharRaoSiripuram <shashi@zplus.care>
2025-07-17 14:52:23 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./{app,components,libs,pages,hooks}/**/*.{html,js,ts,jsx,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./hooks/**/*.{js,ts,jsx,tsx}", // Only if you use hooks folder
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
